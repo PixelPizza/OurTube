74b6c2b717c8d71cb237f3546cadb89368fe61f0
Jaron <60853956+JaronZ@users.noreply.github.com>
2021-08-29 00:41:05 +0200

    Feat(Commands): Add invite command (#8)

---
+++ src/commands/invite.ts
@@ -0,0 +1,33 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { stripIndents } from "common-tags";
+import { CommandInteraction, MessageEmbed } from "discord.js";
+import { SlashCommand } from "../command";
+
+module.exports = class extends SlashCommand {
+	constructor(){
+		super(new SlashCommandBuilder()
+			.setName("invite")
+			.setDescription("get the invite link of the bot"))
+	}
+
+	run(interaction: CommandInteraction){
+		const {client} = interaction;
+		interaction.editReply({
+			embeds: [
+				new MessageEmbed({
+					color: "BLUE",
+					title: "OurTube invite links",
+					description: stripIndents`
+						[Recommended](${client.generateInvite({
+							scopes: ["bot"]
+						})})
+						[Admin](${client.generateInvite({
+							scopes: ["bot"],
+							permissions: ["ADMINISTRATOR"]
+						})})
+					`
+				})
+			]
+		});
+	}
+}
