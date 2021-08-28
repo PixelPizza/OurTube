40e1af342c6a181b7baa14356fb50590ecc7b8d6
Jaron <60853956+JaronZ@users.noreply.github.com>
2021-08-28 21:31:48 +0200

    Fix(CustomConsole): Remove logs of token (#5)

--- src/console.ts
+++ src/console.ts
@@ -6,10 +6,16 @@ config();
 class CustomConsole {
 	private static webhook: WebhookClient = new WebhookClient({url: process.env.CONSOLE_URL});
 	static log(...params: any[]){
+		const {TOKEN} = process.env;
 		console.log(...params);
 		this.webhook.send({
 			embeds: params.map(param => new MessageEmbed({
-				description: typeof(param) == "string" ? param : `\`\`\`ts\n${inspect(param)}\`\`\``
+				description: (typeof(param) == "string" ? param : `\`\`\`ts\n${inspect(param)}\`\`\``)
+					.replaceAll(TOKEN, "BOT_TOKEN")
+					.replaceAll(TOKEN
+						.split('.')
+						.map((val, i) => (i > 1 ? val.replace(/./g, '*') : val))
+						.join('.'), "BOT_TOKEN")
 			}))
 		});
 	}
