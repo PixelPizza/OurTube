80ba5bc5c8eb33fb99760c781c25f61869aba01c
Jaron <60853956+JaronZ@users.noreply.github.com>
2021-08-28 21:21:15 +0200

    Fix(InteractionCreate): Errors get shown to users (#4)

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -14,12 +14,13 @@ module.exports = class extends ClientEvent {
 
 		if(!command) return;
 
+		if(command.defer) await interaction.deferReply({ephemeral: command.ephemeral});
+
 		try {
-			if(command.defer) await interaction.deferReply({ephemeral: command.ephemeral});
 			await command.run(interaction);
 		} catch (error) {
 			CustomConsole.log(error);
-			await interaction.reply({content: "There was an error while executing this command!", ephemeral: true});
+			await interaction.editReply({content: "There was an error while executing this command!", /*ephemeral: true*/});
 		}
 	}
 }
