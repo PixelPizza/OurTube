1ffa0cbe746d430d521470e291c9485ff0f9452b
Jaron <60853956+JaronZ@users.noreply.github.com>
2021-08-29 22:39:43 +0200

    Feat: Auto-Reload Code (#19)
    * Merge master into auto-reload-code (#18)
    
    * Feat: Add join command (#15)
    
    * Feat(Commands): Added join command
    
    * Fix(Client): Add missing client.settings
    
    * Feat: Removed startNoBuild from scripts (#16)
    
    * Fix(Invite): Added applications.commands scope (#17)
    
    * Feat: remove watch.ts
    
    * Feat: Added custom command/event reloading

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,5 @@
 import { VoiceConnection } from "@discordjs/voice";
-import { Client, ClientEvents, Collection } from "discord.js";
+import { Awaited, Client, ClientEvents, Collection } from "discord.js";
 import { SlashCommand } from "./command";
 interface GuildSettings {
 	connection: VoiceConnection | null;
@@ -23,6 +23,25 @@ class CustomClient<Ready extends boolean = boolean> extends Client<Ready> {
 	 * The settings of the client per guild
 	 */
 	public readonly settings: Collection<string, GuildSettings> = new Collection();
+
+	private readonly events: Collection<string, {
+		name: string,
+		listener: (...args: any[]) => any
+	}> = new Collection();
+
+	public onCustom<K extends keyof ClientEvents>(file: string, event: K, listener: (...args: ClientEvents[K]) => Awaited<void>, once: boolean = false): this {
+		this.events.set(file, {
+			name: event,
+			listener
+		});
+		return once ? this.once(event, listener) : this.on(event, listener);
+	}
+
+	public offCustom(file: string): this {
+		const oldEvent = this.events.get(file);
+		this.events.delete(file);
+		return this.off(oldEvent.name, oldEvent.listener);
+	}
 }
 
 /**
