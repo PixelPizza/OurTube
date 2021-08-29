7692c3d63fd560f798eb0666e7b6146574a0f1dc
Jaron <60853956+JaronZ@users.noreply.github.com>
2021-08-29 14:07:42 +0200

    Feat(Commands): Made seperate file for registering commands (#11)

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,5 +1,3 @@
-import {REST} from "@discordjs/rest";
-import { Routes } from "discord-api-types/v9";
 import { ActivityOptions } from "discord.js";
 import { ClientEvent, CustomClient } from "../client";
 import { CustomConsole } from "../console";
@@ -10,40 +8,10 @@ module.exports = class extends ClientEvent {
 	}
 
 	async run(client: CustomClient<true>){
-		const rest = new REST({version: "9"}).setToken(client.token),
-			guildId = process.env.GUILD;
-
-		try {
-			const {user, commands} = client;
-
-			CustomConsole.log(`Started refreshing ${commands.size} application (/) commands.`);
-
-			const clientId = user.id,
-				[globalCommands, guildCommands] = commands.partition(command => command.global);
-
-			CustomConsole.log(`Refreshing ${globalCommands.size} global (/) commands.`);
-
-			await rest.put(
-				Routes.applicationCommands(clientId),
-				{body: globalCommands.map(command => command.data.toJSON())}
-			);
-
-			CustomConsole.log(`Refreshing ${guildCommands.size} guild (/) commands.`);
-
-			await rest.put(
-				Routes.applicationGuildCommands(clientId, guildId),
-				{body: guildCommands.map(command => command.data.toJSON())}
-			);
-
-			CustomConsole.log(`Successfully reloaded ${commands.size} application (/) commands.`);
-		} catch (error) {
-			CustomConsole.log(error);
-		}
-
 		try {
 			CustomConsole.log("Setting default permission for guild (/) commands.");
 
-			for(const command of (await (await client.guilds.fetch(guildId)).commands.fetch()).values()){
+			for(const command of (await (await client.guilds.fetch(process.env.GUILD)).commands.fetch()).values()){
 				await command.permissions.add({
 					permissions: [
 						{
