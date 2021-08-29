63c2bea06145190c3ec39cbbefb7192cdb8dfb12
Jaron <60853956+JaronZ@users.noreply.github.com>
2021-08-29 12:10:03 +0200

    Docs: Added JSDoc comments to classes (#10)

--- src/client.ts
+++ src/client.ts
@@ -1,19 +1,42 @@
 import { Client, ClientEvents, Collection } from "discord.js";
 import { SlashCommand } from "./command";
 
+/**
+ * An extended client class
+ */
 class CustomClient<Ready extends boolean = boolean> extends Client<Ready> {
+	/**
+	 * The commands of the client
+	 */
 	public readonly commands: Collection<string, SlashCommand> = new Collection();
 }
 
+/**
+ * Base class for client events
+ */
 abstract class ClientEvent {
+	/**
+	 * The name of the event
+	 */
 	public readonly name: keyof ClientEvents;
+	/**
+	 * If this event should only be run once
+	 */
 	public readonly once: boolean;
 
+	/**
+	 * Make a new client event
+	 * @param name The name of the event
+	 * @param once If this event should only be run once
+	 */
 	public constructor(name: keyof ClientEvents, once?: boolean){
 		this.name = name;
 		this.once = once ?? false;
 	}
 
+	/**
+	 * Run this event
+	 */
 	public abstract run(...args: unknown[]): void;
 }
 
--- src/console.ts
+++ src/console.ts
@@ -3,9 +3,22 @@ import { MessageEmbed, WebhookClient } from "discord.js";
 import {inspect} from "util";
 config();
 
+/**
+ * A custom console that automatically sends the output to discord
+ */
 class CustomConsole {
+	/**
+	 * The webhook to use when sending logs
+	 */
 	private static webhook: WebhookClient = new WebhookClient({url: process.env.CONSOLE_URL});
-	static log(...params: any[]){
+
+	/**
+	 * Log something to the console and discord
+	 * 
+	 * Replaces the token of the bot with `BOT_TOKEN`
+	 * @param params The params to log
+	 */
+	public static log(...params: any[]){
 		const {TOKEN} = process.env;
 		console.log(...params);
 		this.webhook.send({
