319d44cadb905485c7d9e1038222e1284be5e10f
Jaron <jaron_zijlstra@outlook.com>
2021-08-30 21:55:03 +0200

    Feat(NowPlaying): Made nowplaying ephemeral

--- src/commands/nowPlaying.ts
+++ src/commands/nowPlaying.ts
@@ -11,7 +11,6 @@ module.exports = class extends SlashCommand {
 			.setDescription("show the current playing song"),
 		{
 			guildOnly: true,
-			ephemeral: false,
 			needsVoiceChannel: true
 		});
 	}
