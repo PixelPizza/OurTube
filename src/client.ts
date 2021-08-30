de9fd0cae27f6233b4561984e3e0bf3d470695ae
Jaron <60853956+JaronZ@users.noreply.github.com>
2021-08-30 15:48:19 +0200

    Feat(Commands): Added play command (#21)

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,6 @@
 import { VoiceConnection } from "@discordjs/voice";
-import { Awaited, Client, ClientEvents, Collection } from "discord.js";
+import { Player } from "discord-player";
+import { Awaited, Client, ClientEvents, ClientOptions, Collection } from "discord.js";
 import { SlashCommand } from "./command";
 interface GuildSettings {
 	connection: VoiceConnection | null;
@@ -23,11 +24,20 @@ class CustomClient<Ready extends boolean = boolean> extends Client<Ready> {
 	 * The settings of the client per guild
 	 */
 	public readonly settings: Collection<string, GuildSettings> = new Collection();
+	/**
+	 * The player used to play songs
+	 */
+	public readonly player: Player;
 
 	private readonly events: Collection<string, {
 		name: string,
 		listener: (...args: any[]) => any
 	}> = new Collection();
+
+	constructor(options: ClientOptions){
+		super(options);
+		this.player = new Player(this);
+	}
 
 	public onCustom<K extends keyof ClientEvents>(file: string, event: K, listener: (...args: ClientEvents[K]) => Awaited<void>, once: boolean = false): this {
 		this.events.set(file, {
---
+++ src/commands/play.ts
@@ -0,0 +1,75 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { QueryType } from "discord-player";
+import { CommandInteraction, GuildMember, MessageEmbed } from "discord.js";
+import { CustomClient } from "../client";
+import { SlashCommand } from "../command";
+
+module.exports = class extends SlashCommand {
+	constructor(){
+		super(new SlashCommandBuilder()
+			.setName("play")
+			.setDescription("play a song")
+			.addStringOption(option => option
+				.setName("query")
+				.setDescription("the song to play")
+				.setRequired(true)),
+		{
+			guildOnly: true,
+			ephemeral: false,
+			needsVoiceChannel: true
+		})
+	}
+
+	async run(interaction: CommandInteraction){
+		const client = interaction.client as CustomClient<true>,
+			query = interaction.options.getString("query", true),
+			result = await client.player.search(query, {
+				requestedBy: interaction.user,
+				searchEngine: QueryType.AUTO
+			}),
+			{guild} = interaction;
+
+		if(!result || !result.tracks.length) return void interaction.editReply({
+			embeds: [
+				new MessageEmbed({
+					color: "RED",
+					title: "No song found",
+					description: "No results were found"
+				})
+			]
+		});
+
+		const queue = client.player.createQueue(guild, {
+			metadata: interaction.channel
+		});
+
+		try {
+			if (!queue.connection) await queue.connect((interaction.member as GuildMember).voice.channel);
+		} catch {
+			client.player.deleteQueue(guild);
+			return void interaction.editReply({
+				embeds: [
+					new MessageEmbed({
+						color: "RED",
+						title: "Can't join voice channel",
+						description: "Unable to join your voice channel"
+					})
+				]
+			});
+		}
+
+		const type = result.playlist ? "playlist" : "song";
+
+		await interaction.editReply({
+			embeds: [
+				new MessageEmbed({
+					color: "BLUE",
+					title: `Loading ${type}`,
+					description: `Now loading your requested ${type}`
+				})
+			]
+		});
+		queue.addTracks(result.tracks);
+		if(!queue.playing) await queue.play();
+	}
+}
