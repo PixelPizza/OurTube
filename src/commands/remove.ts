e4f97f0df8ae839424972c8ca790b080cb94c829
Jaron <60853956+JaronZ@users.noreply.github.com>
2021-10-27 15:57:54 +0200

    feat: add remove command (#50)

---
+++ src/commands/remove.ts
@@ -0,0 +1,40 @@
+import {SlashCommand} from "discord-extend";
+import {CommandInteraction, MessageEmbed} from "discord.js";
+import {CustomClient} from "../client";
+
+module.exports = class extends SlashCommand {
+	constructor() {
+		super({
+			name: "remove",
+			description: "remove a song from the current queue",
+			options: [
+				{
+					type: "INTEGER",
+					name: "index",
+					description: "The queue index of the song to remove",
+					required: true
+				}
+			],
+			checks: ["guildOnly", "botInVoice", "inSameVoice"]
+		});
+	}
+
+	async run(interaction: CommandInteraction) {
+		await interaction.deferReply({ephemeral: true});
+
+		const client = interaction.client as CustomClient<true>,
+			queue = client.player.getQueue(interaction.guild),
+			index = interaction.options.getInteger("index"),
+			removed = queue.remove(index-1);
+
+		interaction.editReply({
+			embeds: [
+				new MessageEmbed({
+					color: "GREEN",
+					title: "Remove",
+					description: `Removed [${removed.title}](${removed.url}) from the queue at index ${index}`
+				})
+			]
+		});
+	}
+};
