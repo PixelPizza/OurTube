c1ce9643ab779216b6e0588d2f112e23e27c6f23
Jaron <jaron_zijlstra@outlook.com>
2021-12-07 15:35:22 +0100

    feat: change discord-extend client to sapphire

--- src/bot.ts
+++ src/bot.ts
@@ -1,20 +1,14 @@
-import {SlashCommandCheck, Client} from "discord-extend";
 import {config} from "dotenv";
-import {join} from "path";
 import {PlayerEvent} from "./event";
 import {Util} from "./util";
 import {Player} from "discord-player";
-import {container} from "@sapphire/framework";
+import {container, SapphireClient} from "@sapphire/framework";
 config();
 
-const client = new Client({
+const client = new SapphireClient({
 	intents: ["GUILDS", "GUILD_VOICE_STATES"]
-}).addEventsIn(join(__dirname, "events/client"));
-container.player = new Player(this);
-
-client.registry
-	.registerCommandsIn(join(__dirname, "commands"))
-	.registerCommandChecks(...Object.values(SlashCommandCheck.DEFAULT));
+});
+container.player = new Player(client);
 
 Util.getJSFiles("events/player", (events: PlayerEvent[], files: string[]) => {
 	events.forEach(event => event.once ? container.player.once(event.name, event.run) : container.player.on(event.name, event.run));
