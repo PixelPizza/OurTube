781b60579ec71d1b4c50edfba6d2050411a72c93
mcpsbot <83839790+mcpsbot@users.noreply.github.com>
2021-12-11 22:16:22 +0000

    Update clear.ts

--- src/commands/clear.ts
+++ src/commands/clear.ts
@@ -1,49 +1,26 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
-
 import { ApplyOptions } from "@sapphire/decorators";
-
 import { ApplicationCommandRegistry, Command, CommandOptions } from "@sapphire/framework";
-
 import { CommandInteraction, MessageEmbed } from "discord.js";
-
 @ApplyOptions<CommandOptions>({
-
-    description: "clear the queue"
-
+    description: "clear the queue",
+    preconditions: ["GuildOnly", "BotInVoice", "InSameVoice"]
 })
-
 export class ClearCommand extends Command {
-
     public registerApplicationCommands(registry: ApplicationCommandRegistry) {
-
         registry.registerChatInputCommand(new SlashCommandBuilder().setName(this.name).setDescription(this.description));
-
     }
-
     public async chatInputRun(interaction: CommandInteraction) {
-
         await interaction.deferReply();
-
         this.container.player.getQueue(interaction.guild).clear();
-
         interaction.editReply({
-
             embeds: [
-
                 new MessageEmbed({
-
                     color: "GREEN",
-
                     title: "Queue cleared",
-
                     description: "The queue has been cleared."
-
                 })
-
             ]
-
         });
-
     }
-
 }
