b8a9891836e82abe013e5f280f58b7d145249d8e
Jaron <jaron_zijlstra@outlook.com>
2022-03-30 21:24:27 +0200

    fix: compile languages to json

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import type { Options } from "tsup";
 export const tsup: Options = {
 	clean: true,
 	dts: false,
-	entryPoints: ["src/sharder.ts", "src/bot.ts", "src/commands/**/*.ts", "src/listeners/**/*.ts", "src/languages/**/*.json", "src/preconditions/**/*.ts"],
+	entryPoints: ["src/sharder.ts", "src/bot.ts", "src/commands/**/*.ts", "src/listeners/**/*.ts", "src/preconditions/**/*.ts"],
 	format: ["esm"],
 	minify: true,
 	skipNodeModulesBundle: true,
