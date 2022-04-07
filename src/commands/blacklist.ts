44bdd85aedb187ad3acbf3622155ce3dcb7f6828
Jaron <60853956+JaronZ@users.noreply.github.com>
2022-04-07 21:55:46 +0200

    feat: add unblacklist (#85)
    * feat: move to subcommand
    
    * feat: add unblacklist

--- src/commands/blacklist.ts
+++ src/commands/blacklist.ts
@@ -13,19 +13,42 @@ export class BlacklistCommand extends Command {
 			new SlashCommandBuilder()
 				.setName(this.name)
 				.setDescription(this.description)
-				.addUserOption(input => input.setName("user").setDescription("the user to blacklist").setRequired(true))
-				.addStringOption(input =>
-					input.setName("reason").setDescription("reason why for being blacklisted").setRequired(true)
+				.addSubcommand(input =>
+					input
+						.setName("add")
+						.setDescription("Add a user to the blacklist")
+						.addUserOption(input =>
+							input.setName("user").setDescription("the user to blacklist").setRequired(true)
+						)
+						.addStringOption(input =>
+							input.setName("reason").setDescription("reason why for being blacklisted").setRequired(true)
+						)
+				)
+				.addSubcommand(input =>
+					input
+						.setName("remove")
+						.setDescription("Remove a user from the blacklist")
+						.addUserOption(input =>
+							input.setName("user").setDescription("the user to unblacklist").setRequired(true)
+						)
 				),
 			{
-				guildIds: ["863878432697614337"]
+				guildIds: ["863878432697614337"],
+				idHints: ["960218037863215195"]
 			}
 		);
 	}
 
-	public async chatInputRun(interaction: CommandInteraction) {
+	public async chatInputRun(interaction: CommandInteraction): Promise<any> {
 		await interaction.deferReply();
 
+		switch (interaction.options.getSubcommand(true)) {
+			case "add":
+				return this.chatInputAdd(interaction);
+		}
+	}
+
+	public async chatInputAdd(interaction: CommandInteraction) {
 		const user = interaction.options.getUser("user", true);
 		const reason = interaction.options.getString("reason", true);
 
@@ -54,6 +77,36 @@ export class BlacklistCommand extends Command {
 					.setColor("GREEN")
 					.setTitle("User blacklisted")
 					.setDescription(`${user} has been blacklisted`)
+			]
+		});
+	}
+
+	public async chatInputRemove(interaction: CommandInteraction) {
+		const user = interaction.options.getUser("user", true);
+
+		if (!(await this.container.prisma.blacklist.findUnique({where: {user: user.id}}))) {
+			return interaction.editReply({
+				embeds: [
+					new MessageEmbed()
+						.setColor("RED")
+						.setTitle("User not blacklisted")
+						.setDescription(`${user} is not blacklisted.`)
+				]
+			});
+		}
+
+		await this.container.prisma.blacklist.delete({
+			where: {
+				user: user.id
+			}
+		});
+
+		return interaction.editReply({
+			embeds: [
+				new MessageEmbed()
+					.setColor("GREEN")
+					.setTitle("User unblacklisted")
+					.setDescription(`${user} has been unblacklisted`)
 			]
 		});
 	}
