51a6dff283cedb698fea33b1be9f3862b972b44e
mcpsbot <83839790+mcpsbot@users.noreply.github.com>
2023-03-10 20:51:03 +0000

    Update queueEnd.ts

--- src/listeners/player/queueEnd.ts
+++ src/listeners/player/queueEnd.ts
@@ -1,13 +1,13 @@
 import {ApplyOptions} from "@sapphire/decorators";
 import {container, Listener} from "@sapphire/framework";
-import {type Queue, Util} from "discord-player";
+import {type GuildQueue, Util} from "discord-player";
 
 @ApplyOptions<Listener.Options>({
 	emitter: container.player,
 	event: "queueEnd"
 })
 export class QueueEndListener extends Listener {
-	public run(GuildQueue: Queue): void {
+	public run(Queue: Queue): void {
 		if (Util.isVoiceEmpty(queue.connection.channel)) {
 			queue.delete();
 			this.container.logger.debug(`[${queue.guild.name}] Queue finished!`);
