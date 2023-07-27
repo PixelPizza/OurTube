2f7bda312e728d5f60abfece30a0f604f78dedad
Jaron <jaron_zijlstra@outlook.com>
2023-07-27 23:21:04 +0200

    refactor: use shapeshift for env variables

--- src/augment.d.ts
+++ src/augment.d.ts
@@ -1,11 +1,13 @@
 import type {PrismaClient} from "@prisma/client";
 import {Player} from "discord-player";
+import type {parseEnv} from "./lib/Env";
 
 export * from "@sapphire/pieces";
 declare module "@sapphire/pieces" {
 	export interface Container {
 		player: Player;
 		prisma: PrismaClient;
+		env: ReturnType<typeof parseEnv>;
 	}
 }
 
