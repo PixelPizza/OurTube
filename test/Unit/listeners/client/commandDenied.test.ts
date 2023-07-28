418efec8a0fbd2a8e033939c86f16fdf351040ab
Jaron <jaron_zijlstra@outlook.com>
2023-07-28 13:21:39 +0200

    test(CommandDeniedListener): add run test

---
+++ test/Unit/listeners/client/commandDenied.test.ts
@@ -0,0 +1,33 @@
+import { container, ListenerStore, UserError } from "@sapphire/framework";
+import { CommandDeniedListener } from "../../../../src";
+import { DiscordMocker } from "../../../DiscordMocker";
+import { Colors, EmbedBuilder } from "discord.js";
+import "@sapphire/plugin-i18next/register";
+
+describe("CommandDeniedListener tests", () => {
+	test("GIVEN error THEN replies to interaction with error message", async () => {
+		const mocker = new DiscordMocker();
+		const store = new ListenerStore();
+		const listener = store.construct(CommandDeniedListener, {
+			path: "",
+			name: "commandDenied",
+			root: "",
+			extension: ".js"
+		});
+		const interaction = mocker.mockInteraction({
+			name: "error"
+		});
+		const error = new UserError({
+			identifier: "error",
+			message: "error"
+		});
+		const spy = vi.spyOn(interaction, "reply");
+		await container.i18n.init();
+
+		await listener.run(error, { interaction });
+
+		expect(spy).toHaveBeenCalledWith({
+			embeds: [new EmbedBuilder().setColor(Colors.Red).setTitle("Command Failed").setDescription("error")]
+		});
+	});
+});
