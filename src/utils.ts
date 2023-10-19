436a53ae6db5ecefb12e231928325045b5b4b3ff
Jaron <jaron_zijlstra@outlook.com>
2023-10-19 17:06:01 +0200

    v2 cleanup

--- src/utils.ts
+++
@@ -1,9 +0,0 @@
-import type { StringMap, Target, TOptions } from "@sapphire/plugin-i18next";
-import { resolveKey } from "@sapphire/plugin-i18next";
-
-export function resolveMaybeKey(target: Target, key: string, options?: TOptions<StringMap>): Promise<string> {
-	return resolveKey(target, key, {
-		...options,
-		defaultValue: key
-	});
-}
